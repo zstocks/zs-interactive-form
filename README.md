e9f95467c7909b6cfdd1554613dceac0a327d62a
Zachary Stocks <stocks.zachary@gmail.com>
2021-09-13 18:56:00 -0600

    Validation Refactoring
    simplified and made more readable the helper functions and form submit event handler.
    
    Reduced amount of repeated code by employing functions, and storing error messages in an errorMessages object.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # zs-interactive-form
  An interactive form using JavaScript
+
+1. Conditional Error Messaging: 
+ Display the appropriate error message/hint-text when a form field is not valid on form submission.
+
+ 1.1 Name
+  Acceptable values: ^[a-zA-Z]+ [a-zA-Z]+$
+  The name field accepts a first and last name separated by a space, consisting of letters.
+
+  Error messages:
+  a) The application first checks to see if the name field is empty - if so, hint-text will read: Name field cannot be blank
+
+  b) Next, the application ensures the user is only entering letters with a space. 
+     If this criteria is not met, the hint-text will read: Please enter first and last name using only letters separated by a space
+
+ 1.2 Email
+  The email field accepts only email addresses that follow the format: string@string.a-z
+  Here is the logic for the regex expression: ^[^@\s]+@[^@.\s]+\.[a-z]+$
+   1. [^@\s]+ - accepts any string of characters that are not '@' or whitespace
+   2. @ - followed by '@' 
+   3. [^@.\s]+ - followed by any string of characters that are not '@', '.', or whitespace
+   4. \.[a-z] - followed by '.' and a string made of letters strictly a-z
+
+   Error messages:
+   a) The application first checks to see if the email field is empty - if so, hint-text will read: Email field cannot be blank
+
+   b) Next, the application ensures the email address meets the required format.
+      If the required format is not met, hint-text will read: Email address must be formatted correctly. ex: name@domain.com
+
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const form = document.querySelector('form');
 const otherJob = document.querySelector('#other-job-role');
 const jobRole = document.querySelector('#title');
 const shirtColor = document.querySelector('#color');
@@ -5,6 +6,11 @@ const shirtDesign = document.querySelector('#design');
 const activities = document.querySelector('#activities');
 let totalCost = 0;
 const activitiesCost = document.querySelector('#activities-cost');
+const paymentType = document.querySelector('#payment');
+const payOptions = paymentType.querySelectorAll('option');
+const credit = document.querySelector('#credit-card');
+const paypal = document.querySelector('#paypal');
+const bitcoin = document.querySelector('#bitcoin');
 
 // focus on name text field when page loads
 document.querySelector('#name').focus();
@@ -63,19 +69,130 @@ activities.addEventListener('change', (e) => {
 });
 
 //*** PAYMENT INFO ***//
-const paymentType = document.querySelector('#payment');
-const payOptions = paymentType.querySelectorAll('option');
-const credit = document.querySelector('#credit-card');
-const paypal = document.querySelector('#paypal');
-const bitcoin = document.querySelector('#bitcoin');
 
-// by default, select the 'credit card' payment method
+/**
+ * Updates the page to show only the selected payment method
+ * 
+ * @param (string) selectedMethod - the payment method selected by the user
+ */
+const updatePayMethod = (selectedMethod) => {
+  if (selectedMethod === 'credit-card') {
+    credit.style.display = 'block';
+    paypal.style.display = 'none';
+    bitcoin.style.display = 'none';
+  } else if (selectedMethod === 'paypal') {
+    paypal.style.display = 'block';
+    credit.style.display = 'none';
+    bitcoin.style.display = 'none';
+  } else if (selectedMethod === 'bitcoin') {
+    bitcoin.style.display = 'block';
+    paypal.style.display = 'none';
+    credit.style.display = 'none';
+  }
+}
+
+// by default, select the 'credit card' payment method and update the form to show only the selected payment method
 payOptions[1].selected = 'selected';
+updatePayMethod(paymentType.value);
 
-// instead of hiding these elements in this way, try
-// building a function that shows the div associated
-// with the selected payment method and hiding the 
-// others. Call that method at runtime, and call it
-// again in the event listener for the dropdown.
-paypal.style.display = 'none';
-bitcoin.style.display = 'none';
+// when the user changes the payment method, update the page to display their selected payment method
+paymentType.addEventListener('change', () => {
+  updatePayMethod(paymentType.value);
+});
+
+//*** VALIDATION ***//
+//*** helper functions to verify form fields ***//
+const isValidName = (name) => {
+  return /^[a-zA-Z]+ [a-zA-Z]+$/.test(name); // see readme section 1.1
+}
+
+const isValidEmail = (email) => {
+  return /^[^@\s]+@[^@.\s]+\.[a-z]+$/i.test(email); // see readme section 1.2
+}
+
+const isValidActivity = () => {
+  // check for selected activities by ensuring the totalCost is greater than 0
+  return totalCost > 0 ? true : false;
+}
+
+const isValidCredit = (ccNumber) => {
+  // a number between 13 and 16 digits
+  return /^\d{13,16}$/.test(ccNumber);
+}
+
+const isValidZip = (zipCode) => {
+  // a number exactly 5 digits
+  return /^\d{5}$/.test(zipCode);
+}
+
+const isValidCvv = (cvv) => {
+  // a number exactly 3 digits
+  return /^\d{3}$/.test(cvv);
+}
+
+form.addEventListener('submit', (e) => {
+  const name = document.querySelector('#name').value;
+  const nameHint = document.querySelector('#name-hint');
+  const email = document.querySelector('#email').value;
+  const emailHint = document.querySelector('#email-hint');
+  const activitiesHint = document.querySelector('#activities-hint');
+  const ccNumber = document.querySelector('#cc-num').value;
+  const ccHint = document.querySelector('#cc-hint');
+  const zip = document.querySelector('#zip').value;
+  const zipHint = document.querySelector('#zip-hint');
+  const cvv = document.querySelector('#cvv').value;
+  const cvvHint = document.querySelector('#cvv-hint');
+  const errorMessages = {
+    name: {
+      validate: isValidName(name),
+      blank: 'Name field cannot be blank',
+      format: 'Please enter first and last name using only letters separated by a space'
+    },
+    email: {
+      validate: isValidEmail(email),
+      blank: 'Email field cannot be blank',
+      format: 'Email address must be formatted correctly. ex: name@domain.com'
+    }
+  }
+
+  const basicValidation = (helperFunction, hint) => {
+    // if helperFunction returns false, display a hint on the page
+    if (!helperFunction) {
+      e.preventDefault();
+      hint.style.display = 'block';
+    } else {
+      // if helperFunction returns true, hide the hint-text
+      hint.style.display = 'none';
+    }
+  }
+
+  const customErrorValidation = (fieldValue, hint, type) => {
+    if (!type.validate) {
+      e.preventDefault();
+      if (fieldValue === '') {
+        hint.innerText = type.blank;
+      } else {
+        hint.innerText = type.format;
+      }
+      hint.style.display = 'block';
+    } else {
+      hint.style.display = 'none';
+    }
+  }
+
+  // validate name and show appropriate error message
+  customErrorValidation(name, nameHint, errorMessages.name);
+
+  // validate email and show appropriate error message
+  customErrorValidation(email, emailHint, errorMessages.email);
+
+  // validate activity
+  basicValidation(isValidActivity(), activitiesHint);
+
+  // validate credit card info only if Credit Card is the selected payment method
+  if (paymentType.value === 'credit-card') {
+    basicValidation(isValidCredit(ccNumber), ccHint);
+    basicValidation(isValidZip(zip), zipHint);
+    basicValidation(isValidCvv(cvv), cvvHint);
+  }
+});
